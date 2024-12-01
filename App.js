7cbbefba637b524283ef290ac17e3522ae4b0137
Aadil-khan <aadilkhan53792@gmail.com>
2024-12-02 00:45:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+
+
+function App(){
+    const [column , setColumn] = useState([])
+    const [records , setRecords] = useState([])
+
+    useEffect(()=> {
+        fetch('http://localhost:3000/Data.json')
+        .then(res => res.json())
+        .then(data => {
+            setColumn(Object.keys(data.users[0]))
+            setRecords(data.users)
+        })
+    }, [])
+    return(
+        <div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                       {column.map((c, i) => (
+                         <th key={i}>{c}</th>
+                       ))} 
+                    </tr>
+                    </thead> 
+                    <tbody>
+                        {
+                            records.map((record, i) =>{
+                                <tr key={i}>
+                                    <td>{record.id}</td>
+                                    <td>{record.name}</td>
+                                    <td>{record.email}</td>
+
+                                </tr>
+                            })
+                        }
+                    </tbody>
+            </table>
+        </div>
+    )
+
+}
